36efcf0354bdb336d32f65f87643ce1d32213db5
Jeroen P. Broks <facebook@tbbs.nl>
2020-04-10 13:37:55 +0200

    License in source

--- src/TBFC.cpp
+++ src/TBFC.cpp
@@ -1,5 +1,28 @@
-// TBCF.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lic:
+// Tricky's BrainFuck Compiler
+// Translates Brainfuck code into C
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 20.04.10
+// EndLic
 
 #include <iostream>
 #include <string>
@@ -63,14 +86,3 @@ int main(int acount,char** args) {
 		Translate(args[i]);
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
